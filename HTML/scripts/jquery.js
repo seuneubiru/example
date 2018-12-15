da7cedd31808541247550b210c9db9a1770dd0a1
@mufiq.patoni <mufiqpatoni.aommalik@gmail.com>
2018-12-15 13:50:20 +0700

    add few fiel

---
+++ HTML/scripts/jquery.js
@@ -0,0 +1,39 @@
+
+var input = document.getElementById("back");
+
+$(input).click(function(){
+    alert("kembali ke Home");
+});
+
+
+
+var x = 4;
+
+var y = "14";
+
+var z = x + y;
+
+var a = x * y;
+
+console.log(x);
+console.log(y);
+
+console.log(z);
+console.log(a);
+
+var a = new Array(10);
+
+a[50] = 17;
+
+for (let index = 0; index < a.length; index++) {
+    console.log(index[a]);
+}
+
+
+var x = 10;
+
+console.log(++x);
+console.log(x++);
+console.log(x);
+
+
---
+++ HTML/scripts/latihan.js
@@ -0,0 +1,18 @@
+var tambah = document.getElementById("btn");
+var timpah = document.getElementById("test");
+
+$(tambah).click(function(){
+    var addbtn = document.createElement("label");
+    var nama = document.createTextNode("Username");
+    var addinput =  document.createElement("input");
+    div.innerHTML= "username";
+})
+
+
+$(tambah).click(function(){
+    document.get
+});
+
+// $(tambah).click(function() {
+//     alert("test");
+// });
